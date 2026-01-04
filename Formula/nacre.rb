69edcc88ec52435ae74835d2e9edb2cbaa0f6e9a
Istvan <dev@l1x.be>
2026-01-04 23:24:40 +0100

    release 0.9.2

--- Formula/nacre.rb
+++ Formula/nacre.rb
@@ -1,17 +1,19 @@
-class Nacre < Formula
-  desc "Agentic project management UI"
-  homepage "https://github.com/l1x/nacre"
-  url "https://github.com/l1x/nacre/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "88b673ac72e52d97f7d2081ed73181c95ef07fa52245eb4f482fa51640051f34"
-  license "MIT"
+  class Nacre < Formula
+    desc "Agentic project management UI"
+    homepage "https://github.com/l1x/nacre"
+    version "0.9.2"
+    license "MIT"
 
-  depends_on "rust" => :build
+    depends_on arch: :arm64
 
-  def install
-    system "cargo", "install", *std_cargo_args
+    url "https://github.com/l1x/nacre/releases/download/v0.9.2/nacre-aarch64-apple-darwin.tar.gz"
+    sha256 "722a06192dc56f0ad73a6e9db3f4fde93fa2c22038e43663eac3487186014007"
+
+    def install
+      bin.install "nacre"
+    end
+
+    test do
+      assert_match "nacre", shell_output("#{bin}/nacre --version")
+    end
   end
-
-  test do
-    assert_match "nacre", shell_output("#{bin}/nacre --version")
-  end
-end
