4138f41f33850d0fe360de9a553ff76c438fc881
Istvan <dev@l1x.be>
2026-01-05 17:31:25 +0100

    nacre 0.9.3

--- Formula/nacre.rb
+++ Formula/nacre.rb
@@ -1,13 +1,13 @@
   class Nacre < Formula
     desc "Agentic project management UI"
     homepage "https://github.com/l1x/nacre"
-    version "0.9.2"
+    version "0.9.3"
     license "MIT"
 
     depends_on arch: :arm64
 
-    url "https://github.com/l1x/nacre/releases/download/v0.9.2/nacre-aarch64-apple-darwin.tar.gz"
-    sha256 "722a06192dc56f0ad73a6e9db3f4fde93fa2c22038e43663eac3487186014007"
+    url "https://github.com/l1x/nacre/releases/download/v0.9.3/nacre-aarch64-apple-darwin.tar.gz"
+    sha256 "7bf44a2a125e1184c8c62b53fc3b86577abe6648e169dc6cd5ff038973e04353"
 
     def install
       bin.install "nacre"
